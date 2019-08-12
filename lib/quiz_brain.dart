404e822023d1eb300330b310ed77b7e1708b745f
Rashid Razak <bib.nexus@gmail.com>
2019-08-13 01:36:02 +0800

    Add launcher icons for iOS and Android.

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -5,7 +5,7 @@ class QuizBrain {
   int _score = 0;
 
   List<Question> _questionBank = [
-    Question('Some cats are actually allergic to humans', true),
+    Question('Some cats are actually allergic to humans.', true),
     Question('You can lead a cow down stairs but not up stairs.', false),
     Question('Approximately one quarter of human bones are in the feet.', true),
     Question('A slug\'s blood is green.', true),
